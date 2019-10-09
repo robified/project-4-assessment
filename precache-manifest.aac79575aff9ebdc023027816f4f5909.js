841afe4a2d037428ccc9054a075088a83e75a70d
Robin <daorob@gmail.com>
2019-10-09 11:29:55 -0500

    Updates

---
+++ precache-manifest.aac79575aff9ebdc023027816f4f5909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357d36e804eb1abf3eee2f172022993f",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "5dde49619eea03474bf0",
+    "url": "/project-4-assessment/static/css/main.ac1bc663.chunk.css"
+  },
+  {
+    "revision": "f3546792c885bad3c856",
+    "url": "/project-4-assessment/static/js/2.91efb0bf.chunk.js"
+  },
+  {
+    "revision": "5dde49619eea03474bf0",
+    "url": "/project-4-assessment/static/js/main.420bdf33.chunk.js"
+  },
+  {
+    "revision": "75de8ec33a8a78802050",
+    "url": "/project-4-assessment/static/js/runtime-main.71652b97.js"
+  }
+]);
